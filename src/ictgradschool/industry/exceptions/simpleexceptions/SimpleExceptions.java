9e9d8785c40c8abe8cb5e154f159c2d2e5bcb73e
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-26 15:17:05 +1300

    done ex04 1&2

--- src/ictgradschool/industry/exceptions/simpleexceptions/SimpleExceptions.java
+++ src/ictgradschool/industry/exceptions/simpleexceptions/SimpleExceptions.java
@@ -23,17 +23,39 @@ public class SimpleExceptions {
      */
     public void handlingException() {
         Scanner sc = new Scanner(System.in);
+        int num1 = 0;
+        while (true) {
+            System.out.print("Enter the first number: ");
+            String str1 = sc.next();
 
+            try {
+                num1 = Integer.parseInt(str1);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter an integer");
+            }
+        }
 
-        System.out.print("Enter the first number: ");
-        String str1 = sc.next();
-        int num1 = Integer.parseInt(str1);
+    // Output the result
+    while (true) {
         System.out.print("Enter the second number: ");
         String str2 = sc.next();
-        int num2 = Integer.parseInt(str2);
+        int num2 =0;
+        try {
+            num2 = Integer.parseInt(str2);
+            if(num2!=0){
+                System.out.println("The division of " + num1 + " over " + num2 + " is " + (num1 / num2) + "\n");
+                break;
+            } else {
+                System.out.println("div by 0");
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("Please enter an integer");
+        }
 
-        // Output the result
-        System.out.println("The division of " + num1 + " over " + num2 + " is " + (num1 / num2) + "\n");
+
+
+}
     }
 
     public void Question3() {
